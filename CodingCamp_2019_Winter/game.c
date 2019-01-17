24cc9ca04e0949f6e46bbde5f1654ff68d8a9c44
dslab <46699096+dslab-ysj@users.noreply.github.com>
2019-01-17 09:59:57 +0900

    Update game.c

--- CodingCamp_2019_Winter/game.c
+++ CodingCamp_2019_Winter/game.c
@@ -13,22 +13,19 @@
  * functions for games
  ************************************************/
 
-//**********³ª¸¸ÀÇ ¾Ç±â I¿¡¼­ »ç¿ë**********//
-//pwm Á¦¾î ÇÔ¼ö
 void pwm_func(int gpio_pin, int period, int duty_cycle, char* name)
 {
 	int PWM_PIN = PWM0;
 	int fd = pwm_open(PWM_PIN);
 	printf("%s\n", name);
 	pwm_write(fd,period,duty_cycle);
-	while(gpio_read(gpio_pin)){}   //switch°¡ ´­·ÁÀÖ´Â µ¿¾È while¹® ¾È¿¡ ¸Ó¹°±â
+	while(gpio_read(gpio_pin)){}   
 	pwm_close(fd);
 	return;
 }
-//**********³ª¸¸ÀÇ ¾Ç±â I¿¡¼­ »ç¿ë**********//
 
 
-//**********2Áø¼ö °ÔÀÓ**********//
+
 int binary_main(int argc, FAR char *argv[])
 {
    int sw_left = PIN_D2;
@@ -52,10 +49,7 @@ int binary_main(int argc, FAR char *argv[])
    }
    return 0;
 }
-//**********2Áø¼ö °ÔÀÓ**********//
 
-
-//**********°¨¼ºÀÚ±Ø ¿À¸£°ñ**********//
 int mp3_main(int argc, FAR char *argv[])
 {
 	int play_sw = PIN_D2;
@@ -109,9 +103,7 @@ int mp3_main(int argc, FAR char *argv[])
 
 	return 0;
 }
-//**********°¨¼ºÀÚ±Ø ¿À¸£°ñ**********//
 
-//**********³ª¸¸ÀÇ ¾Ç±â I**********//
 
 int piano_main(int argc, FAR char *argv[])
 {
@@ -129,7 +121,7 @@ int piano_main(int argc, FAR char *argv[])
    int sw_MI_val = 0;
    int sw_SO_val = 0;
 
-   //switch Ã³À½ ´­¸² °¨Áö
+   //switch ì²˜ìŒ ëˆŒë¦¼ ê°ì§€
    while(1)
    {
 	  sw_DO_val = gpio_read(sw_DO);
@@ -148,9 +140,6 @@ int piano_main(int argc, FAR char *argv[])
    return 0;
 }
 
-//**********³ª¸¸ÀÇ ¾Ç±â I**********//
-
-//**********Åõ¸íÀÚ**********//
 
 int ruler_main(int argc, FAR char *argv[])
 {
@@ -177,22 +166,18 @@ int ruler_main(int argc, FAR char *argv[])
 	}
 	return 0;
 }
-//**********Åõ¸íÀÚ**********//
 
-
-//**********³ª¸¸ÀÇ ¾Ç±â II**********//
 int inst2_main(int argc, FAR char *argv[])
 {
 	int low_tone_sw  = 0;
 	int high_tone_sw = 0;
 	int pin_ir = 0;
 	int pin_buzzer = 0;
-	int val_low_tone_sw = 0;
-	int val_high_tone_sw = 0;
 	int val_ir = 0;
 	int fd_buzzer = 0;
 	int TONE = 0;
 	int Duty_Cycle = 100;
+	int TONE_var = 0;
 
 	low_tone_sw  = PIN_D2; // GPIO 0 PORT
 	high_tone_sw = PIN_D4;
@@ -203,47 +188,138 @@ int inst2_main(int argc, FAR char *argv[])
 
 	while(1)
 	{
-		val_low_tone_sw = gpio_read(low_tone_sw);
-		val_high_tone_sw = gpio_read(high_tone_sw);
 		val_ir = read_adc(pin_ir);
-		up_mdelay(100);
 
-		if((val_low_tone_sw == HIGH)&&(val_high_tone_sw == LOW))
-		{
-			fd_buzzer = pwm_open(pin_buzzer);
-			if(val_ir>550) 		TONE = O5_DO;
-			else if(val_ir>250) TONE = O5_RE;
-			else if(val_ir>140) TONE = O5_MI;
-			else if(val_ir>105) TONE = O5_FA;
-			else 				TONE = O5_SO;
-			pwm_write(fd_buzzer,TONE,Duty_Cycle);
-			up_mdelay(100);
-			pwm_close(fd_buzzer);
+		if(gpio_read(high_tone_sw)) {
+		      if(val_ir>550)
+		    	  TONE = E7;
+		      else if(val_ir>250)
+		    	  TONE = D7;
+		      else if(val_ir>140)
+		    	  TONE = C7;
+		      else if(val_ir>105)
+		    	  TONE = B6;
+		      else
+		    	  TONE = A6;
+		} else {
+		      if(val_ir>550)
+		    	  TONE = C6;
+		      else if(val_ir>250)
+		    	  TONE = D6;
+		      else if(val_ir>140)
+		    	  TONE = E6;
+		      else if(val_ir>105)
+		    	  TONE = F6;
+		      else
+		    	  TONE = G6;
 		}
-		else if ((val_low_tone_sw == LOW)&&(val_high_tone_sw == HIGH))
-		{
-			fd_buzzer = pwm_open(pin_buzzer);
-			if(val_ir>550) 		TONE = O5_LA;
-			else if(val_ir>250) TONE = O5_TI;
-			else if(val_ir>140) TONE = O6_DO;
-			else if(val_ir>105) TONE = O6_RE;
-			else 				TONE = O6_MI;
-			pwm_write(fd_buzzer,TONE,Duty_Cycle);
-			up_mdelay(100);
-			pwm_close(fd_buzzer);
 
-		}
-		else if ((val_low_tone_sw == HIGH)&&(val_high_tone_sw == HIGH))
-		{
-			break;
-		}
-		else{ }
+		if(gpio_read(low_tone_sw) || gpio_read(high_tone_sw)){
+
+			fd_buzzer = pwm_open(pin_buzzer);
+
+			pwm_write(fd_buzzer,TONE,100);
+
+			TONE_var = TONE;
+
+			while(TONE==TONE_var && (gpio_read(low_tone_sw) || gpio_read(high_tone_sw))){
+
+				val_ir = read_adc(adc_pinnum);
+
+				if(gpio_read(SW_high)) {
+					  if(val_ir>550)
+						  TONE_var = E7;
+					  else if(val_ir>250)
+						  TONE_var = D7;
+					  else if(val_ir>140)
+						  TONE_var = C7;
+					  else if(val_ir>105)
+						  TONE_var = B6;
+					  else
+						  TONE_var = A6;
+				} else {
+					  if(ir_val>550)
+						  TONE_var = C6;
+					  else if(ir_val>250)
+						  TONE_var = D6;
+					  else if(ir_val>140)
+						  TONE_var = E6;
+					  else if(ir_val>105)
+						  TONE_var = F6;
+					  else
+						  TONE_var = G6;
+				}
+			}
+				pwm_close(fd_buzzer);
+			}
+
+			else
+			while(gpio_read(low_tone_sw) == 0 && gpio_read(high_tone_sw) == 0);
 	}
 	return 0;
 }
-//**********³ª¸¸ÀÇ ¾Ç±â II**********//
 
 
+int inst3_main(int argc, FAR char *argv[]){
+
+	int pin_buzzer = 0;
+	int SW_low_tone = 0;
+	int SW_high_tone = 0;
+	int SW_exit1 = 0;
+	int SW_exit2 = 0;
+	int sound_low = 0;
+	int sound_high = 0;
+	int fd_buzzer = 0;
+
+	pin_buzzer = PWM0;
+	SW_low_tone = PIN_D2;
+	SW_high_tone = PIN_D7;
+	SW_exit1 = PIN_D4;
+	SW_exit2 = PIN_D8;
+
+	if(atoi(argv[2]) == 1){
+		sound_low = C6;
+		sound_high = D6;
+		printf("C,D\n");
+	} else if(atoi(argv[2]) == 2){
+		sound_low = E6;
+		sound_high = F6;
+		printf("E,F\n");
+	} else if(atoi(argv[2]) == 3){
+		sound_low = G6;
+		sound_high = A6;
+		printf("G,A\n");
+	} else {
+		sound_low = B6;
+		sound_high = C7;
+		printf("B,C\n");
+	}
+
+	while(gpio_read(SW_exit1) == 0 || gpio_read(SW_exit2) == 0){
+
+		if(gpio_read(SW_low_tone)){
+
+			fd_buzzer = pwm_open(pin_buzzer);
+
+			pwm_write(fd_buzzer,sound_low,100);
+			printf("%d\n",sound_low);
+
+			while(gpio_read(SW_low_tone) || (gpio_read(SW_low_tone) && gpio_read(SW_high_tone)));
+		} else if(gpio_read(SW_high_tone)){
+
+			fd_buzzer = pwm_open(pin_buzzer);
+
+			pwm_write(fd_buzzer,sound_high,100);
+			printf("%d\n",sound_high);
+
+			while(gpio_read(SW_high_tone) || (gpio_read(SW_low_tone) && gpio_read(SW_high_tone)));
+		}
+
+		pwm_close(fd_buzzer);
+
+	}
+
+}
 
 static void show_game_hexagon(FAR const char *program)
 {
@@ -254,6 +330,7 @@ static void show_game_hexagon(FAR const char *program)
    printf("|             %s instrument1              |\n", program);
    printf("|               %s ruler                  |\n", program);
    printf("|             %s instrument2              |\n", program);
+   printf("|             %s instrument3 (1,2,3,4)    |\n", program);
    printf("|-------------------------------------------|\n");
 
 }
@@ -267,6 +344,7 @@ int game_main(int argc, FAR char *argv[])
    else if ( (argc==2) && (strcmp(argv[1],"instrument1")==0) ) {piano_main(argc,argv);}
    else if ( (argc==2) && (strcmp(argv[1],"ruler")==0) ) {ruler_main(argc,argv);}
    else if ( (argc==2) && (strcmp(argv[1],"instrument2")==0) ) {inst2_main(argc,argv);}
+   else if ( (argc==3) && (strcmp(argv[1],"instrument4")==0) ) {inst3_main(argc,argv);}
    else show_game_hexagon(argv[0]);
 
    return 0;
